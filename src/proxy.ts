f196751067b18312dcfa61128e4371a41746d07a
AkshatJain <jainakshat_02@protonmail.com>
2026-01-28 17:08:19 +0530

    added clerk auth

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,6 +2,11 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import createDatabase from './models/server/dbSetup'
 import {createStorageBucket} from './models/server/storage.collection'
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+
+
+export default clerkMiddleware();
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
@@ -22,6 +27,11 @@ export const config = {
    */
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
+
 
 }  
---
+++ src/store/Auth.ts
@@ -0,0 +1,3 @@
+import {create} from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { persist } from "zustand/middleware";
